59d7f8d34fd8d35a5b71675fb3a093d3cf8557fc
Brent Brown <brent.brown@gmail.com>
2017-02-28 13:59:27 -0700

    Messing with scope.

--- main/foo.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func otherPrint() {
-	fmt.Printf("%v - %T from otherPrint() \n", h, h)
-}
--- main/main.go
+++ main/main.go
@@ -2,36 +2,40 @@ package main
 
 import (
 	"fmt"
-	"stringutils"
 )
 
-var h string = "hi"
+var x = 0
 
 func main() {
 
-	a := 10
-	b := "golang"
-	c := 4.17
-	d := true
-	e := "Hello"
-	f := `Do you like my hat?`
-	g := 'M'
-	//a = 11
-	//h := 12
+	x := 40
 
-	fmt.Printf("%v - %T \n", a, a)
-	fmt.Printf("%v - %T \n", b, b)
-	fmt.Printf("%v - %T \n", c, c)
-	fmt.Printf("%v - %T \n", d, d)
-	fmt.Printf("%v - %T \n", e, e)
-	fmt.Printf("%v - %T \n", f, f)
-	fmt.Printf("%v - %T \n", g, g)
-	//fmt.Printf("%v - %T \n", h, h)
-	otherPrint()
+	foo := func() {
+		fmt.Println(x)
+	}
 
-	fmt.Println(stringutils.Reverse("Hi There!"))
+	fmt.Println(x)
+	{
+		x := 1
+		y := x
+		fmt.Println(x)
+		{
+			fmt.Println(y)
+			foo()
+		}
 
-	max := max(7)
-	fmt.Println(max)
-	fmt.Printf("%v - %T \n", X, X)
+		increment := wrapper()
+		fmt.Println(increment())
+		fmt.Println(increment())
+	}
 }
+
+
+func wrapper() func() int {
+	//x := 10
+	return func() int {
+		x++
+		return x
+	}
+}
+
