5749c264835a202901bc97ebbe99dbc95cf65240
brent brown <brentbrown@gmail.com>
2017-02-27 20:54:42 -0700

    Formatting numbers.

--- main/main.go
+++ main/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+
+	for index := 1; index < 256; index++ {
+		fmt.Printf("%d \t %b \t %x \n", index, index, index)
+	}
 }
