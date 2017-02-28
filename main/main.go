f0aaff49bd19fd916c2a2c4b785d4335881bce75
Brent Brown <brent.brown@gmail.com>
2017-02-28 11:35:32 -0700

    Messing with variables.

--- main/main.go
+++ main/main.go
@@ -5,10 +5,28 @@ import (
 	"stringutils"
 )
 
-//import "../stringutils"
-//import "github.com/BrentBrown/GoSandbox/stringutils"
+var h string = "hi"
 
 func main() {
+
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	e := "Hello"
+	f := `Do you like my hat?`
+	g := 'M'
+	//a = 11
+	//h := 12
+
+	fmt.Printf("%v - %T \n", a, a)
+	fmt.Printf("%v - %T \n", b, b)
+	fmt.Printf("%v - %T \n", c, c)
+	fmt.Printf("%v - %T \n", d, d)
+	fmt.Printf("%v - %T \n", e, e)
+	fmt.Printf("%v - %T \n", f, f)
+	fmt.Printf("%v - %T \n", g, g)
+	fmt.Printf("%v - %T \n", h, h)
 
 	fmt.Println(stringutils.Reverse("Hi There!"))
 }
