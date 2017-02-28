9ca0820bb8af503163452b9824804f853e3b1058
Brent Brown <brent.brown@gmail.com>
2017-02-28 15:04:38 -0700

    Named returns.

--- main/main.go
+++ main/main.go
@@ -4,38 +4,13 @@ import (
 	"fmt"
 )
 
-var x = 0
-
 func main() {
+	fmt.Println(foo(2))
 
-	x := 40
-
-	foo := func() {
-		fmt.Println(x)
-	}
-
-	fmt.Println(x)
-	{
-		x := 1
-		y := x
-		fmt.Println(x)
-		{
-			fmt.Println(y)
-			foo()
-		}
-
-		increment := wrapper()
-		fmt.Println(increment())
-		fmt.Println(increment())
-	}
 }
 
-
-func wrapper() func() int {
-	//x := 10
-	return func() int {
-		x++
-		return x
-	}
+func foo(x int) (y, z int) {
+	y = x * 10
+	z = x * 20
+	return
 }
-
