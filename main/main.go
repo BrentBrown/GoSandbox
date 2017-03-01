f31ad188a0ae82a6b6a294532077385ac3ecfee6
brent brown <brentbrown@gmail.com>
2017-02-28 22:03:22 -0700

    Pointers

--- main/main.go
+++ main/main.go
@@ -5,7 +5,20 @@ import (
 )
 
 func main() {
-	var foo int
-	fmt.Scan(&foo)
-	fmt.Println(foo)
+	a := 42
+
+	fmt.Printf("%v - %T \n", a, a)
+	fmt.Printf("%v - %T \n", &a, &a)
+
+	var b *int = &a
+	fmt.Printf("%v - %T \n", b, b)
+	fmt.Printf("%v - %T \n", *b, *b)
+
+	*b = 27
+	fmt.Printf("%v - %T \n", a, a)
+
+	*&a = 12
+	fmt.Printf("%v - %T \n", *b, *b)
+
+
 }
