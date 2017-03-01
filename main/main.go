17afb1a586a50d1cf64f31ef07722001892541ef
brent brown <brentbrown@gmail.com>
2017-02-28 21:48:32 -0700

    Scan to memory location.

--- main/main.go
+++ main/main.go
@@ -5,18 +5,7 @@ import (
 )
 
 func main() {
-	c, d := foo(2)
-	fmt.Println(c)
-	fmt.Println(&c)
-	fmt.Println(&d)
-	temp := foo
-	fmt.Println(&temp)
-	fmt.Printf("%d \n", &temp)
-	fmt.Printf("%b \n", &temp)
-}
-
-func foo(x int) (y, z int) {
-	y = x * 10
-	z = x * 20
-	return
+	var foo int
+	fmt.Scan(&foo)
+	fmt.Println(foo)
 }
