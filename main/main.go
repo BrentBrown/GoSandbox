1b6b2b2b1fedf0b8d40eeab39a6e31e387fe6001
Brent Brown <brent.brown@gmail.com>
2017-03-01 09:02:45 -0700

    More passing pointer stuff.

--- main/main.go
+++ main/main.go
@@ -4,23 +4,25 @@ import (
 	"fmt"
 )
 
-
 func zero(x int) {
-	fmt.Printf("%v - %T \n", x, x)
+	fmt.Printf("zero x value - type: %v - %T \n", x, x)
+	fmt.Printf("zero x address - type: %v - %T \n", &x, &x)
 	x = 0
 }
 
-func zerop(x *int) {
-	fmt.Printf("%v - %T \n", x, x)
+func zeropointer(x *int) {
+	fmt.Printf("zeropointer x value - type: %v - %T \n", x, x)
 	*x = 0
 }
 
 func main() {
 	x := 5
-	fmt.Printf("%v - %T \n", x, x)
-	zero(x)
-	fmt.Printf("%v - %T \n", x, x)
+	fmt.Printf("main x value - type: %v - %T \n", x, x)
+	fmt.Printf("main x address - type: %v - %T \n", &x, &x)
 
-	zerop(&x)
-	fmt.Printf("%v - %T \n", x, x)
+	zero(x)
+	fmt.Printf("main x value - type after call to zero - type: %v - %T \n", x, x)
+
+	zeropointer(&x)
+	fmt.Printf("main x value - type after call to zeropointer: %v - %T \n", x, x)
 }
