4d67ebd4439bcac4ae9af50ee968cc52fb566d5a
Brent Brown <brentbrown@gmail.com>
2017-03-02 16:44:21 -0700

    Variadic parameters.

--- variadic/main/main.go
+++ variadic/main/main.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	n := average(233, 432434, 223, 967867, 54674)
-	fmt.Println(n)
+
+	fmt.Println(average(233, 432434, 223, 967867, 54674))
+
+	s := []float64{233, 432434, 223, 967867, 54674}
+	fmt.Println(average(s...))
 }
 
 func average(sf ...float64) float64 {
-	fmt.Println(sf)
-	fmt.Printf("%T \n", sf)
+	//fmt.Println(sf)
+	//fmt.Printf("%T \n", sf)
 	var total float64
 	for _, v := range sf {
 		total += v
