44704e1672025745fc95799d9d8cade43ace0991
Brent Brown <brentbrown@gmail.com>
2017-03-03 10:14:47 -0700

    callback

--- callback/main/main.go
+++ callback/main/main.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
-func visit(numbers []int, callback func(int)) {
+func filter(numbers []int, callback func(int) bool) []int {
+	var xs []int
 	for _, n := range numbers {
-		callback(n)
+		if callback(n) {
+			xs = append(xs, n)
+		}
 	}
+	return xs
 }
 
 func main() {
-	visit([]int{1, 2, 3, 4}, func(n int) {
-		fmt.Println(n)
-	})
+	a := func(n int) bool {
+		return n > 1
+	}
+	xs := filter([]int{1, 2, 3, 4}, a)
+	fmt.Println(xs) // [2 3 4]
 }
