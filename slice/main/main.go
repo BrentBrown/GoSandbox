5cb4bbb1fde5a0d957c9d9ba44584c5a8a06aa77
brent brown <brentbrown@gmail.com>
2017-03-03 20:10:21 -0700

    slice

---
+++ slice/main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var student []string
+	var student = make([]string, 0, 200)
+	
+	println(len(student))
+	println(cap(student))
+
+	student = append(student, "Cyclops")
+	println(len(student))
+	println(cap(student))
+
+	student = append(student, "Rogue")
+	println(len(student))
+	println(cap(student))
+
+	student = append(student, "Jean")
+	println(len(student))
+	println(cap(student))
+
+	student = append(student, "Logan")
+	println(len(student))
+	println(cap(student))
+
+	fmt.Println(student)
+}
