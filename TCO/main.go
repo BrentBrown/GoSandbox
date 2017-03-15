91dffa9515ea09db47f2a6d1c441c3fdc89835b7
Brent Brown <brentbrown@gmail.com>
2017-03-15 16:50:44 -0600

    TCO

---
+++ TCO/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	answer := recursive(4)
+	fmt.Printf("Recursive: %d\n", answer)
+
+	tailAnswer := tailRecursive(4, 1)
+	fmt.Printf("Tail Recursive: %d\n", tailAnswer)
+
+	result := make(chan int)
+	recursiveChannel(4, 1, result)
+	channelAnswer := <-result
+	fmt.Printf("Channel Recursive: %d\n", channelAnswer)
+}
+
+func recursive(number int) int {
+	if number == 1 {
+		return number
+	}
+	return number * recursive(number-1)
+}
+
+//compiler doesn't actually optimize stack.
+func tailRecursive(number int, product int) int {
+	product = product * number
+
+	if number == 1 {
+		return product
+	}
+	return tailRecursive(number-1, product)
+}
+
+func recursiveChannel(number int, product int, result chan int) {
+	product = product * number
+
+	if number == 1 {
+		result <- product
+		return
+	}
+	go recursiveChannel(number-1, product, result)
+}
