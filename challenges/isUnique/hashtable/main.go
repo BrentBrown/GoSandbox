b7cbd0dede02a99f33781f3a1baf640fbe17ec29
Brent Brown <brentbrown@gmail.com>
2017-03-17 15:02:59 -0600

    setting up challenges

---
+++ challenges/isUnique/hashtable/main.go
@@ -0,0 +1,7 @@
+package main
+
+//Is Unique: Implement an algorithm  to  determine  if  a string has  all  unique characters.
+
+func main() {
+
+}
--- slice/main.go
+++ slice/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	//var student []string
-	var student = make([]string, 0, 1)
+	var student = make([]string, 0, 1) //length, capacity
 	//var student = make([]string, 0, 200)
 
 	println(len(student))
