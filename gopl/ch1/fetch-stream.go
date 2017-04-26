ffb733eb5483e9ad40d109683e61e28cfefda89e
Brent Brown <brentbrown@gmail.com>
2017-04-26 17:06:56 -0600

    fetch

--- gopl/ch1/fetch-stream.go
+++ gopl/ch1/fetch-stream.go
@@ -5,10 +5,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprint(os.Stderr, "fetch: %v\n", err)
@@ -20,6 +24,7 @@ func main() {
 			fmt.Fprint(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		fmt.Println(resp.Status)
 	}
 
 }
