e56f976c1778e8f73e1cc1e99a05756ed381e6c6
Brent Brown <brentbrown@gmail.com>
2018-02-27 05:26:36 +0000

    Adding multiple passes.

--- array/main/main.go
+++ array/main/main.go
@@ -1,24 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
-	const s = 20
-	var as, bs int
-	var a, b [s]int
-
-	o := getdata(s)
-
-	for i, j := 0, len(o)-1; j >= 0; i, j = i+1, j-1 {
-		a[i], b[j] = as+o[i], bs+o[j]
-		as, bs = as+o[i], bs+o[j]
-	}
-	fmt.Println(o)
-	fmt.Println(a)
-	fmt.Println(b)
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s", name, elapsed)
+	fmt.Println()
 }
 
-func getdata(s int) []int {
+func main() {
+	defer timeTrack(time.Now(), "main")
+	const s = 10
+	const n = 1
+	var as, bs int
+	var a, b [s]int
+	var p = false
+
+	for z := 0; z < n; z++ {
+		o := getOriginalData(s)
+
+		for i, j := 0, len(o)-1; j >= 0; i, j = i+1, j-1 {
+			a[i], b[j] = as+o[i], bs+o[j]
+			as, bs = as+o[i], bs+o[j]
+		}
+
+		if p {
+			fmt.Println(o)
+			fmt.Println(a)
+			fmt.Println(b)
+			fmt.Println()
+		}
+	}
+
+}
+
+func getOriginalData(s int) []int {
 	o := make([]int, s)
 	for i, j := 0, s-1; i <= j; i, j = i+1, j-1 {
 		o[i], o[j] = i+1, j+1
