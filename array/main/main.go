2f80805f303209d83e72676bc9a810a67348b978
Brent Brown <brentbrown@gmail.com>
2018-02-27 02:55:57 +0000

    Playing with arrays

---
+++ array/main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	const s = 9
+	var as, bs int
+	var a, b [s]int
+
+	o := getdata(s)
+
+	for i, j := 0, len(o)-1; j >= 0; i, j = i+1, j-1 {
+		a[i], b[j] = as+o[i], bs+o[j]
+		as, bs = as+o[i], bs+o[j]
+	}
+	fmt.Println(o)
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
+func getdata(s int) []int {
+	o := make([]int, s)
+	for i, j := 0, s-1; i <= j; i, j = i+1, j-1 {
+		o[i], o[j] = i+1, j+1
+	}
+	return o
+}
+
+// func getdataOld(s int) []int {
+// 	o := make([]int, s)
+// 	for i := 0; i < s; i++ {
+// 		o[i] = i + 1
+// 	}
+// 	return o
+// }
